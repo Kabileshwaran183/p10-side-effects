7851a44f8a97423504be694ca4ebf27830aff372
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-04-30 13:31:37 +0530

    format-d\

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,27 @@ export default function App() {
     const [starWarsData, setStarWarsData] = React.useState({})
     const [count, setCount] = React.useState(0)
     const [show, setShow] = React.useState(true)
-    
+
     function toggle() {
         setShow(prevShow => !prevShow)
     }
-    
+
     console.log("Rendered!")
-    
+
     React.useEffect(() => {
         console.log("Effect ran")
         fetch("https://swapi.dev/api/people/1")
             .then(res => res.json())
             .then(data => setStarWarsData(data))
     }, []) // [] compared to []
-    
+
     return (
         <div>
             <main className="container">
-                        <button onClick={toggle}>
-                            Toggle WindowTracker
-                        </button>
-                        {show && <WindowTracker />}
+                <button onClick={toggle}>
+                    Toggle WindowTracker
+                </button>
+                {show && <WindowTracker />}
             </main>
             <h2>The count is {count}</h2>
             <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
--- src/WindowTracker.jsx
+++ src/WindowTracker.jsx
@@ -2,19 +2,19 @@ import React from "react"
 
 export default function WindowTracker() {
     const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
-    
+
     React.useEffect(() => {
-        function watchWindowWidth () {
+        function watchWindowWidth() {
             console.log("Resized")
             setWindowWidth(window.innerWidth)
         }
         window.addEventListener("resize", watchWindowWidth)
-        return function() {
+        return function () {
             console.log("Cleaning up...")
             window.removeEventListener("resize", watchWindowWidth)
         }
     }, [])
-    
+
     return (
         <h1>Window width: {windowWidth}</h1>
     )
